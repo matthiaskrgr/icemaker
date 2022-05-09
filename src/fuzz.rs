6b12e05a1678e33789fd5257ce68009f27cfc8b3
Matthias Krüger <matthias.krueger@famsik.de>
2022-05-09 20:25:05 +0200

    fuzz: type shenanigans

--- src/fuzz.rs
+++ src/fuzz.rs
@@ -1,5 +1,4 @@
 use rand::prelude::IteratorRandom;
-use rand::Rng;
 
 static RANDOM_ITEMS: &[&str] = &[
     " main ",
@@ -66,7 +65,10 @@ static RANDOM_ITEMS: &[&str] = &[
     "0",
 ];
 
+/*
 pub(crate) fn get_random_string() -> String {
+    use rand::Rng;
+
     const MAX_ITEMS_PER_FILE: usize = 10;
     let mut rng = rand::thread_rng();
 
@@ -80,4 +82,72 @@ pub(crate) fn get_random_string() -> String {
 
     //println!("{}", output);
     output
+}
+
+*/
+
+pub(crate) fn get_random_string() -> String {
+    dbg!(gen_random_main())
+}
+
+/// get one random identifier
+fn r() -> &'static str {
+    let mut rng = rand::thread_rng();
+    RANDOM_ITEMS.iter().choose(&mut rng).unwrap()
+}
+
+fn _gen_random_fn() -> String {
+    format!(
+        "pub fn fooo<{} , {}>(s: &String) -> () {{   todo!() }}\n",
+        r(),
+        r(),
+    )
+}
+
+/// generate a main with a binding to r()
+fn gen_random_main() -> String {
+    format!(
+        "pub fn main() {{
+  let x =   {};
+  let z = {}
+    
+}}",
+        r(),
+        tc()
+    )
+}
+
+static TYPES_SIMPLE: &[&str] = &[
+    "String::new()",
+    "Vec::<u32>::new()",
+    "std::path::PathBuf",
+    "u32",
+    "i8",
+    "f32",
+    "&str",
+    "usize",
+    "None",
+    "Option<i32>",
+];
+
+static TYPES_COMPLEXE: &[&str] = &[
+    "Vec::<TY>::new()",
+    //"Option<T>",
+    //"Result<T,T>",
+    "Some(TY)",
+    "Ok(TY)",
+    "Err(TY)",
+    "[TY]",
+];
+
+// get a simple type
+fn ts() -> &'static str {
+    let mut rng = rand::thread_rng();
+    TYPES_SIMPLE.iter().choose(&mut rng).unwrap()
+}
+
+fn tc() -> String {
+    let mut rng = rand::thread_rng();
+    let type_raw = TYPES_COMPLEXE.iter().choose(&mut rng).unwrap().to_string();
+    type_raw.replace("TY", ts())
 }

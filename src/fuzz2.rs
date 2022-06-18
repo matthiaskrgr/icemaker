2f5cc17600f96acefea04af63017b59a13759feb
Matthias Krüger <matthias.krueger@famsik.de>
2022-06-18 23:32:28 +0200

    impl Display for Stuff

--- src/fuzz2.rs
+++ src/fuzz2.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 struct FunctionGenerator<'a> {
     id: usize,
     // keep a list of generated functions so we can reference them in other functions..?
@@ -39,6 +41,25 @@ enum Ty {
     String,
 }
 
+impl std::fmt::Display for Ty {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let x = match self {
+            Self::u8 => "u8",
+            Self::u16 => "u16",
+            Self::u32 => "u32",
+            Self::u64 => "u64",
+            Self::i8 => "i8",
+            Self::i16 => "i16",
+            Self::i32 => "i32",
+            Self::i64 => "i64",
+            Self::usize => "usize",
+            Self::isize => "isize",
+            Self::String => "String",
+        };
+        write!(f, "{}", x)
+    }
+}
+
 struct Function {
     name: String,
     return_ty: Ty,
@@ -46,4 +67,29 @@ struct Function {
     body: String,
 }
 
+impl std::fmt::Display for Function {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let args_fmtd = self
+            .args
+            .iter()
+            .enumerate()
+            .map(|(i, arg_ty)| format!("arg_{i}: {arg_ty}, "))
+            .collect::<String>();
+        let body = &self.body;
+        write!(f, "fn {}({}) -> {{ {body} }}", &self.name, args_fmtd)
+    }
+}
+
+impl Function {
+    fn to_string(&self) -> String {
+        let args_fmtd = self
+            .args
+            .iter()
+            .enumerate()
+            .map(|(i, arg_ty)| format!("arg_{i}: {arg_ty}, "))
+            .collect::<String>();
+        let body = &self.body;
+        format!("fn {}({}) -> {{ {body} }}", &self.name, args_fmtd)
+    }
+}
 pub(crate) fn fuzz2main() {}

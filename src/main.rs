97a899d5b7183eaa824e3aac53970f008cf475bd
Matthias Krüger <matthias.krueger@famsik.de>
2022-08-27 21:14:16 +0200

    recognize linker errors as ICES Fixes #22

--- src/main.rs
+++ src/main.rs
@@ -873,7 +873,8 @@ fn find_ICE_string(executable: &Executable, output: Output) -> Option<String> {
             "^fatal runtime error: stack overflow",
             "^Unusual: ",
             "^Undefined behavior:",
-            "(SIGABRT)"
+            "(SIGABRT)",
+            "error: linking with"
         ],
     };
 

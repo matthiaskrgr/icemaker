b030544a155051a733647a6a025b1223ab07857f
Matthias Krüger <matthias.krueger@famsik.de>
2022-11-18 16:56:21 +0100

    fuzz2: rm junk

--- src/fuzz2/fuzz2.rs
+++ src/fuzz2/fuzz2.rs
@@ -8,8 +8,9 @@ pub(crate) fn fuzz2main() {
 
     let mut output = String::from("pub fn main() {");
 
-    const MAX_FNS: u32 = 1000;
+    const MAX_FNS: u32 = 10;
 
+    // generate an arbitrary number of functions
     for _ in 0..MAX_FNS {
         let fun = fngen.gen_fn();
         let fun_call = fun.gen_call();
@@ -25,10 +26,4 @@ pub(crate) fn fuzz2main() {
     let mut file = File::create("out.rs").unwrap_or(File::open("out.rs").unwrap());
     file.write_all(output.as_bytes())
         .expect("failed to write to file");
-}
-
-// lets us
-trait Fuzzable {
-    fn insert_pre(&self) -> String;
-    fn insert_post(&self) -> String;
 }

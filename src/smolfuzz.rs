fedd1a321260378306bd336e6eafcd390234969b
Matthias Krüger <matthias.krueger@famsik.de>
2022-11-20 17:00:05 +0100

    fixup

--- src/smolfuzz.rs
+++ src/smolfuzz.rs
@@ -16,17 +16,14 @@ fn variations_up_to_length(items: &[char]) -> impl Iterator<Item = String> + '_ 
 pub(crate) fn gen_smol_code_char_set() -> impl Iterator<Item = String> {
     const CHARS: &[char] = &['0', '1', '[', ']', ',', '#', 'a', 'b', 'c', '=', ':', ';'];
 
-    const SNIPPET_LENGTH: usize = 5;
+    // snippet will have length of the input charscd /
+    //const SNIPPET_LENGTH: usize = 5;
     const ITEM_LIMIT: usize = 10_000;
 
     variations_up_to_length(CHARS).take(ITEM_LIMIT)
 }
 
 pub(crate) fn codegen_smolfuzz() {
-    let x = gen_smol_code_char_set().collect::<Vec<_>>();
-    dbg!(x);
-    panic!();
-
     gen_smol_code_char_set().enumerate().for_each(|(id, code)| {
         let mut file =
             std::fs::File::create(std::path::PathBuf::from(format!("{}.rs", id))).unwrap();

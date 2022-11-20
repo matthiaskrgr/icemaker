74e171d6e874485fbc4bd4a60a286b1a619d71c1
Matthias Krüger <matthias.krueger@famsik.de>
2022-11-20 16:58:19 +0100

    smolfuzz: use proper solution taken from https://users.rust-lang.org/t/how-can-i-create-a-function-with-all-permutations-of-all-digits-up-to-the-number-of-permutations-asked/75675

--- src/smolfuzz.rs
+++ src/smolfuzz.rs
@@ -1,16 +1,25 @@
 use itertools::Itertools;
 use std::io::prelude::*;
 
+/// https://users.rust-lang.org/t/how-can-i-create-a-function-with-all-permutations-of-all-digits-up-to-the-number-of-permutations-asked/75675/2
+fn variations_up_to_length(items: &[char]) -> impl Iterator<Item = String> + '_ {
+    (1..=items.len())
+        .flat_map(|n| {
+            std::iter::repeat(items.iter())
+                .take(n)
+                .multi_cartesian_product()
+        })
+        .map(|v| v.into_iter().collect::<String>())
+        .filter(|x| x.len() == items.len())
+}
+
 pub(crate) fn gen_smol_code_char_set() -> impl Iterator<Item = String> {
     const CHARS: &[char] = &['0', '1', '[', ']', ',', '#', 'a', 'b', 'c', '=', ':', ';'];
 
     const SNIPPET_LENGTH: usize = 5;
     const ITEM_LIMIT: usize = 10_000;
 
-    (0..=CHARS.len() + 3)
-        .map(|combinations| CHARS.iter().combinations(combinations  ).collect::<Vec<_>>())
-        .flatten()
-        .map(|x| x.into_iter().collect::<String>())
+    variations_up_to_length(CHARS).take(ITEM_LIMIT)
 }
 
 pub(crate) fn codegen_smolfuzz() {

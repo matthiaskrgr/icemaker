a01c415408c5453bf8bb33d34f86057c3d5f1714
Matthias Krüger <matthias.krueger@famsik.de>
2022-11-22 23:47:20 +0100

    ignore hang (rustdoc)

--- src/flags.rs
+++ src/flags.rs
@@ -3148,6 +3148,7 @@ pub(crate) static EXCEPTIONS: &[&str] = &[
     "library/stdarch/crates/core_arch/src/x86/avx512f.rs",
     "src/test/ui/structs-enums/struct-rec/issue-84611.rs",
     "src/test/ui/structs-enums/struct-rec/issue-74224.rs",
+    "src/test/ui/dropck/dropck_no_diverge_on_nonregular_3.rs"
 ];
 
 pub(crate) static MIRI_EXCEPTIONS: &[&str] = &[

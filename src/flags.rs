024abd7a0ae330d5d8798e8c1f6345642afbd7d1
Matthias Krüger <matthias.krueger@famsik.de>
2023-02-16 22:50:05 +0100

    remove -Zsave-analyis

--- src/flags.rs
+++ src/flags.rs
@@ -2,7 +2,7 @@ use crate::library::Args;
 use clap::Parser;
 use once_cell::sync::Lazy;
 
-// -Zvalidate-mir -Zverify-llvm-ir=yes -Zincremental-verify-ich=yes -Zmir-opt-level=0 -Zmir-opt-level=1 -Zmir-opt-level=2 -Zmir-opt-level=3 -Zdump-mir=all --emit=mir -Zsave-analysis -Zprint-mono-items=eager
+// -Zvalidate-mir -Zverify-llvm-ir=yes -Zincremental-verify-ich=yes -Zmir-opt-level=0 -Zmir-opt-level=1 -Zmir-opt-level=2 -Zmir-opt-level=3 -Zdump-mir=all --emit=mir -Zprint-mono-items=eager
 //&q["-Zcrate-attr=feature(generic_associated_types)"],
 // git grep -o  "unstable(feature = \"[A-Za-z_-]*"   | grep -o "\ .*$" | grep -o "\".*" | sed s/\"// | sort -n | uniq | grep "...."
 
@@ -70,7 +70,6 @@ static DEFAULT_RUSTFLAGS: &[&[&str]] = &[
         "-Zunsound-mir-opts",
         "-Zdump-mir=all",
         "--emit=mir",
-        //  "-Zsave-analysis",
         "-Zprint-mono-items=eager",
         "-Zpolymorphize=on",
         "-Zalways-encode-mir",
@@ -98,7 +97,6 @@ static DEFAULT_RUSTFLAGS: &[&[&str]] = &[
         "-Zunsound-mir-opts",
         "-Zdump-mir=all",
         "--emit=mir",
-        //  "-Zsave-analysis",
         "-Zprint-mono-items=eager",
         "-Zpolymorphize=on",
         "-Zalways-encode-mir",
@@ -126,7 +124,6 @@ static DEFAULT_RUSTFLAGS: &[&[&str]] = &[
         "-Zunsound-mir-opts",
         "-Zdump-mir=all",
         "--emit=mir",
-        // "-Zsave-analysis",
         "-Zprint-mono-items=eager",
         "-Zpolymorphize=on",
         "-Zalways-encode-mir",
@@ -219,7 +216,6 @@ static EXPENSIVE_RUSTFLAGS: &[&[&str]] = &[
         "-Zunsound-mir-opts",
         "-Zdump-mir=all",
         "--emit=mir",
-        // "-Zsave-analysis",
         "-Zprint-mono-items=eager",
         "-Zpolymorphize=on",
         "-Zalways-encode-mir",

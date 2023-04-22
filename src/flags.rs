2cb615f329d6082e71c35015282abea14b645772
Matthias Krüger <matthias.krueger@famsik.de>
2023-04-22 17:49:55 +0200

    Revert "EXPERIMENTAL: try to force mir inlining (may only work if mir-opt-level is >= 3 anyway)"
    This reverts commit cc43e9e66e7c1b233328b1d8a328091d3a9c2f58.

--- src/flags.rs
+++ src/flags.rs
@@ -85,8 +85,6 @@ static DEFAULT_RUSTFLAGS: &[&[&str]] = &[
         "-Zmaximal-hir-to-mir-coverage",
         "-Zstrict-init-checks=yes",
         // "-Zlower-impl-trait-in-trait-to-assoc-ty",
-        "-Zinline-mir-threshold=10000",
-        "-Zinline-mir-hint-threshold=10000",
     ],
     // basic stuff, edition 2018
     &[
@@ -121,8 +119,6 @@ static DEFAULT_RUSTFLAGS: &[&[&str]] = &[
         "-Zcrate-attr=feature(impl_trait_in_assoc_type)",
         "-Zcrate-attr=feature(transmute_generic_consts)",
         "-Zcrate-attr=feature(fn_ptr_trait)",
-        "-Zinline-mir-threshold=10000",
-        "-Zinline-mir-hint-threshold=10000",
     ],
     // basic stuff, edition 2021
     &[
@@ -162,8 +158,6 @@ static DEFAULT_RUSTFLAGS: &[&[&str]] = &[
         "-Zcrate-attr=feature(transmute_generic_consts)",
         "-Zcrate-attr=feature(fn_ptr_trait)",
         "-Zstrict-init-checks=yes",
-        "-Zinline-mir-threshold=10000",
-        "-Zinline-mir-hint-threshold=10000",
         // "-Zlower-impl-trait-in-trait-to-assoc-ty",
     ],
     // incremental compilation, keep this!

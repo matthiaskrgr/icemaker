1728b92ee3647a829cca524d05517e722bc34159
Matthias Krüger <matthias.krueger@famsik.de>
2023-11-21 20:25:00 +0100

    Revert "detect "cycle detected when.." diagnostics as ices"
    This reverts commit ba693329a60633d4816eb46a11148d7e390caa8c.

--- src/main.rs
+++ src/main.rs
@@ -1741,8 +1741,6 @@ fn find_ICE_string(
         "left behind trailing whitespace",
         "cycle encountered after",
         "error: rustc interrupted by",
-        "cycle detected when",
-
 
       //  "we would appreciate a bug report",
 
